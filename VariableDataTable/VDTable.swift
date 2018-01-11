085bb2bdf3187365960a6c746061898b38305020
Vojtech Florko <Vojtech.Florko@sk.ibm.com>
2018-01-11 12:53:15 +0100

    Bugs fixed in VDTable

--- VariableDataTable/VDTable.swift
+++ VariableDataTable/VDTable.swift
@@ -1,6 +1,6 @@
 //
 //  VDTable.swift
-//  VariableDataTableDemo
+//  VariableDataTable
 //
 //  Created by Vojtech Florko on 01/08/2017.
 //  Copyright © 2017 Vojtech Florko. All rights reserved.
@@ -16,33 +16,34 @@ public class VDTable: UIView
         super.awakeFromNib()
     }
     
-    //THIS CREATES A UNIVERSAL TABLE WITH n COLUMNS AND n ROWS. NUMBER OF COLUMNS IS COUNT OF COLELEMENTS ARRAY AND NUMBER OF ROWS IS DESCRIBED IN ROWS ARRAY. TYPE COLELEMENTS DESCRIBES A TOP BAR OF COLUMN NAMES AND CONSISTS OF WIDTH AND TEXTVALUE. IF WIDTH IS LESS THAN ZERO, THE WIDTH OF COLUMN NAME WILL BE UNIFORM TO WIDTH OF THE VIEW BY NUMBER OF ELEMENTS, IF NOT, THE WIDTH INPUTTED WILL BE USED.
-    
-    //TBLROW ELEMENTS WILL HAVE AUTOMATIC WIDTHS BASED ON WIDTHS OF COLELEMENTS. TBLROWS CONSIST OF TEXTVALUES ARRAY AND BGCOLOR. IF LESS VALUES THAN COLELEMENTS WILL BE ADDED, THERE WILL BE BLANKS. IF THERE ARE MORE TEXT VALUES, THE ADDITIONAL ONES WILL NOT APPEAR. BGCOLOR DETERMINES THE COLOR OF ROWS.
-    public func assembleHorizontalTable(dataCells: [DataCell], headCells: [HeadCell])
+    public func assembleHorizontalTable(headCells: [HeadCell], dataCells: [DataCell], textAlignment: NSTextAlignment = .left)
     {
         for view in self.subviews
         {
             view.removeFromSuperview()
         }
         
-        let widthOfOneItem = UIScreen.main.bounds.width / (CGFloat(headCells.count))
+        let widthOfOneItem = self.frame.width / (CGFloat(headCells.count))
         var totalWidth: CGFloat = 0.0
         var widths: [CGFloat] = []
         
         let heightOfOneRow = ((self.frame.height) / CGFloat(dataCells.count + 1))
         
-        let headView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: heightOfOneRow))
+        let headView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: heightOfOneRow))
+        headView.backgroundColor = UIColor.white
         
         for i in 0..<headCells.count
         {
             var newItemWidth = widthOfOneItem
-            if headCells[i].dimension >= 0
+            if headCells[i].dimension > 0
             {
                 newItemWidth = headCells[i].dimension
             }
             
-            let headName = UILabel(frame: CGRect(x: totalWidth + CGFloat(20.0), y: 0, width: newItemWidth, height: 25.0))
+            newItemWidth = CGFloat(ceil(Double(newItemWidth)))
+            
+            let headName = UILabel(frame: CGRect(x: totalWidth , y: 0, width: newItemWidth, height: heightOfOneRow))
+            headName.numberOfLines = 4
             
             totalWidth += newItemWidth
             widths.append(newItemWidth)
@@ -50,6 +51,7 @@ public class VDTable: UIView
             headName.font = UIFont(name: headCells[i].fontStyle, size: headCells[i].fontSize)
             headName.textColor = headCells[i].fontColor
             headName.text = headCells[i].textValue
+            headName.textAlignment = textAlignment
             headName.backgroundColor = headCells[i].bgColor
             
             headView.addSubview(headName)
@@ -63,33 +65,33 @@ public class VDTable: UIView
         
         for i in 0..<dataCells.count
         {
-            let dataView = UIView(frame: CGRect(x: 0, y: heightOfOneRow * CGFloat(i) + headView.frame.height, width: UIScreen.main.bounds.width, height: heightOfOneRow))
+            let dataView = UIView(frame: CGRect(x: 0, y: heightOfOneRow * CGFloat(i) + headView.frame.height, width: self.frame.width, height: heightOfOneRow))
             dataView.backgroundColor = dataCells[i].bgColor
             
             var xPosition: CGFloat = 0.0
             
             for j in 0..<widths.count
             {
-                let dataElement = UILabel(frame: CGRect(x: xPosition + CGFloat(20.0), y: 0, width: widths[j], height: heightOfOneRow))
+                widths[j] = CGFloat(ceil(Double(widths[j])))
+                let dataElement = UILabel(frame: CGRect(x: xPosition , y: 0, width: widths[j], height: heightOfOneRow))
                 xPosition += widths[j]
                 
+                dataElement.numberOfLines = 2
                 dataElement.font = UIFont(name: dataCells[i].fontStyle, size: dataCells[i].fontSize)
                 dataElement.textColor = dataCells[i].fontColor
-                dataElement.text = dataCells[i].textValues[j]
+                dataElement.textAlignment = textAlignment
+                if j < dataCells[i].textValues.count
+                {
+                    dataElement.text = dataCells[i].textValues[j]
+                }
                 
                 dataView.addSubview(dataElement)
             }
             self.addSubview(dataView)
         }
-        
-        //        let coverView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
-        //        coverView.backgroundColor = UIColor.yellow
-        //        coverView.alpha = 0.35
-        //        coverView.center = self.center
-        //        self.addSubview(coverView)
     }
     
-    public func assembleVerticalTable(dataCells: [DataCell], headCells: [HeadCell])
+    public func assembleVerticalTable(headCells: [HeadCell], dataCells: [DataCell], textAlignment: NSTextAlignment = .left)
     {
         for view in self.subviews
         {
@@ -104,6 +106,7 @@ public class VDTable: UIView
         let widthOfOneRow = ((self.frame.width) / CGFloat(dataCells.count + 1))
         
         let headView = UIView(frame: CGRect(x: 0, y: 0, width: widthOfOneRow, height: self.frame.height))
+        headView.backgroundColor = UIColor.white
         
         for i in 0..<headCells.count
         {
@@ -113,10 +116,12 @@ public class VDTable: UIView
                 newItemHeight = headCells[i].dimension
             }
             
-            let headName = UILabel(frame: CGRect(x: 0, y: totalHeight + CGFloat(20.0), width: headView.frame.width, height: 25.0))
-            headName.numberOfLines = headCells[i].textValue.characters.count / 2
+            newItemHeight = CGFloat(ceil(Double(newItemHeight)))
             
-            yPositions.append(totalHeight + CGFloat(20.0))
+            let headName = UILabel(frame: CGRect(x: 0, y: totalHeight, width: headView.frame.width, height: newItemHeight))
+            headName.numberOfLines = headCells[i].textValue.count / 2
+            
+            yPositions.append(totalHeight)
             
             totalHeight += newItemHeight
             heights.append(headName.frame.height)
@@ -124,7 +129,7 @@ public class VDTable: UIView
             headName.font = UIFont(name: headCells[i].fontStyle, size: headCells[i].fontSize)
             headName.textColor = headCells[i].fontColor
             headName.text = headCells[i].textValue
-            headName.textAlignment = .center
+            headName.textAlignment = textAlignment
             headName.backgroundColor = headCells[i].bgColor
             
             headView.addSubview(headName)
@@ -145,28 +150,26 @@ public class VDTable: UIView
             
             for j in 0..<heights.count
             {
+                heights[j] = CGFloat(ceil(Double(heights[j])))
                 let dataElement = UILabel(frame: CGRect(x: 0, y: yPositions[j], width: widthOfOneRow, height: heights[j]))
                 yPosition += heights[j]
                 
+                dataElement.numberOfLines = 4
                 dataElement.font = UIFont(name: dataCells[i].fontStyle, size: dataCells[i].fontSize)
                 dataElement.textColor = dataCells[i].fontColor
-                dataElement.text = dataCells[i].textValues[j]
-                dataElement.textAlignment = .center
-                dataElement.numberOfLines = dataElement.text!.characters.count / 2
-                dataElement.backgroundColor = dataCells[i].bgColor
                 
+                if j < dataCells[i].textValues.count
+                {
+                    dataElement.text = dataCells[i].textValues[j]
+                    dataElement.textAlignment = textAlignment
+                    dataElement.numberOfLines = dataElement.text!.count / 2
+                }
+                dataElement.backgroundColor = dataCells[i].bgColor
                 dataView.addSubview(dataElement)
             }
             self.addSubview(dataView)
         }
-        
-        //        let coverView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
-        //        coverView.center = self.center
-        //        coverView.backgroundColor = UIColor.yellow
-        //        coverView.alpha = 0.5
-        //        self.addSubview(coverView)
     }
-    
 }
 
 public class HeadCell
@@ -178,7 +181,7 @@ public class HeadCell
     var fontSize: CGFloat
     var bgColor: UIColor
     
-    public init(textValue: String, dimension: CGFloat = -1, fontStyle: String = "Helvetica", fontColor: UIColor = UIColor.black, fontSize: CGFloat = 15, bgColor: UIColor = UIColor.white)
+    public init(textValue: String, dimension: CGFloat = -1, fontStyle: String = "Helvetica", fontColor: UIColor = UIColor.black, fontSize: CGFloat = 12, bgColor: UIColor = UIColor.white)
     {
         self.textValue = textValue
         self.dimension = dimension
@@ -197,7 +200,7 @@ public class DataCell
     var fontSize: CGFloat
     var bgColor: UIColor
     
-    public init(textValues: [String], fontStyle: String = "Helvetica", fontColor: UIColor = UIColor.black, fontSize: CGFloat = 15, bgColor: UIColor = UIColor.white)
+    public init(textValues: [String], fontStyle: String = "Helvetica", fontColor: UIColor = UIColor.black, fontSize: CGFloat = 12, bgColor: UIColor = UIColor.white)
     {
         self.textValues = textValues
         self.fontStyle = fontStyle
